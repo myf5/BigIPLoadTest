5034aca66ca41e4b74a71951b9aa54233ebe8024
Eric Nelson <eric@sandalsoft.com>
2013-06-28 15:41:25 -0500

    - added virtual server creation (not sure if it's working or taxing the cpu at all)

--- login_bot.py
+++ login_bot.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 
-
+import sys
 import mechanize
 import cookielib
 import time
 import threading
 import Queue
+import string
+import random
 
 bigipIp = "1.1.1.2"
-numClients = 89
+numClients = 5
+vs_IPNetwork = "10.128.10."
 queue = Queue.Queue()
+iid = 1
+iid_lock = threading.Lock()
+
 
 
 class ThreadedClient(threading.Thread):
@@ -20,6 +26,8 @@ class ThreadedClient(threading.Thread):
 		
 	def run(self):
 		# while True:
+
+		clientId = next_id()
 		br = mechanize.Browser()
 
 		# Browser options
@@ -32,42 +40,73 @@ class ThreadedClient(threading.Thread):
 		br.set_handle_redirect(mechanize.HTTPRedirectHandler)
 
 
-		# User-Agent (this is cheating, ok?)
+		# User-Agent
 		br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 
-		br.open("https://1.1.1.2/tmui/login.jsp")
+		br.open("https://" + bigipIp + "/tmui/login.jsp")
 
+##
+#   Login
+#
 
 		br.select_form(name="loginform")
 		# Browser passes through unknown attributes (including methods)
 		# to the selected HTMLForm.
 		br["username"] = "admin"
 		br["passwd"] = "admin"
-		#username
-		#passwd
-
-		redir = br.submit().read()
-		# print cj
-		#https://1.1.1.2/xui/
+		content = br.submit().read()
+		# Simulate pause between clicks
 		time.sleep(.3)
-		r = br.open("https://" + bigipIp + "/xui/")
+##
+#  Open the /xui becuase the script won't parse/interpret the .js
+#
 
+		r = br.open("https://" + bigipIp + "/xui/")
 		content = r.read()
+
+##
+#  Open Local Traffic -> Virtual Servers
+#
+		r = br.open("https://" + bigipIp + "/tmui/Control/jspmap/tmui/locallb/virtual_server/list.jsp?Filter=*")
+		content = r.read()
+##
+# 	Create virtual server
+#
+		r = br.open("https://" + bigipIp + "/tmui/Control/jspmap/tmui/locallb/virtual_server/create.jsp")
+		br.select_form(name="myform")
+		br["name"] = "vs_" + vs_rand_name_generator()
+		br["destination_address_input_vs"] = vs_IPNetwork + str(clientId)
+		br["port"] = "80"
+		content = br.submit().read()
+
+		print "Client " + str(clientId) + " finished"
+		# Write output to file.  probably not needed
 		# with open("mechanize_results.html", "w") as f:
 		# 	f.write(content)
 
+
+
+def next_id():
+	global iid
+	with iid_lock:
+		result = iid
+		iid += 1
+	return result
+
+def vs_rand_name_generator(size=6, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for x in range(size))
 			
-			
+#https://1.1.1.2/tmui/Control/jspmap/tmui/locallb/virtual_server/list.jsp?Filter=*
 
 def main():
-	count = 0
+	client = 1
 	for i in range(numClients):
 		time.sleep(.25)
 		t = ThreadedClient(queue)
 		t.start()
-		print "started client " + str(count)
-		count = count + 1
+		print "started client " + str(client)
+		client = client + 1
 	
 	queue.join()
 main()
